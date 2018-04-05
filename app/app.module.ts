6f07fb8a761a3be6690bb27b6f46abeb43b7c77f
Rahsaan Basek <rahsaanbasek@gmail.com>
2018-04-05 18:30:50 -0400

    update package.json

--- app/app.module.ts
+++ app/app.module.ts
@@ -7,9 +7,11 @@ import {NavbarComponent} from './components/navbar/navbar.component';
 import {JumbotronComponent} from './components/jumbotron/jumbotron.component';
 import {HomeComponent} from './components/pages/home.component';
 import {AboutComponent} from './components/pages/about.component';
+import { routing } from './app.routing';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, routing],
   declarations: [ AppComponent, 
                   NavbarComponent, 
                   JumbotronComponent,
